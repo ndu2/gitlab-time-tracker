e6de105f9e8d8bfbb239401cfb872101b648d1e0
Andreas Müller <blueapu2@gmail.com>
2021-02-25 11:55:13 +0100

    Develop fix month (is 0 based)

--- src/models/hasTimes.js
+++ src/models/hasTimes.js
@@ -24,7 +24,7 @@ class hasTimes extends Base {
      */
     createTime(time, created_at) {
         var date = new Date(created_at);
-        var spentAt = date.getUTCFullYear()+"-"+date.getUTCMonth()+1+"-"+date.getUTCDate();
+        var spentAt = date.getUTCFullYear()+"-"+(date.getUTCMonth()+1)+"-"+date.getUTCDate();
         return this.post(`projects/${this.data.project_id}/${this._type}/${this.iid}/notes`, {
             body: '/spend '+Time.toHumanReadable(time, this.config.get('hoursPerDay'), '[%sign][%days>d ][%hours>h ][%minutes>m ][%seconds>s]'+' '+spentAt),
         });
