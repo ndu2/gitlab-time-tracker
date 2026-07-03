105f5953130545e1458b9aadbe7cd73aab5f4bda
Andreas Müller <andreas@nyquest.ch>
2026-07-03 18:06:43 +0200

    edit multiple records at once

--- src/timekeeping/commands/edit.js
+++ src/timekeeping/commands/edit.js
@@ -5,18 +5,23 @@ import Fs from '../../core/filesystem.js';
 import _ from 'underscore';
 import Time from '../../core/time.js';
 import Frame from '../storage/frame.js';
-import select from '@inquirer/select';
+import select from '@inquirer/checkbox';
+import moment from 'moment';
+import Timekeeper from '../timekeeper.js';
 
 
 
 function edit() {
   const edit = new Command('edit', 'edit time record by the given id')
     .arguments('[id]')
+    .option('-f, --following <number>', 'edit also the following (by ctime) of the given [id]')
+    .option('-n, --listsize <number>', 'list size', 30)
     .action((id, opts ,program) => {
 
 let config = new Config(process.cwd());
 let timeFormat = config.set('timeFormat', program.opts().time_format).get('timeFormat', 'log');
-const listSize = 30;
+let timekeeper = new Timekeeper(config);
+const listSize = program.opts().listsize;
 
 function column(str, n){
     if(str.length > n) {
@@ -29,45 +34,65 @@ function toHumanReadable(input) {
     return Time.toHumanReadable(Math.ceil(input), config.get('hoursPerDay'), timeFormat);
 }
 
-if (!id) {
-  let lastFrames = Fs.all(config.frameDir).slice(-listSize); // last listSize frames (one page of inquirer)
-  lastFrames = lastFrames.map((file) =>
-    Frame.fromFile(config, Fs.join(config.frameDir, file.name))
-  );
-
-  let lastFramesDetails = lastFrames
-    .sort((a, b) => (a.start.isBefore(b.start) ? -1 : 1))
-    .map((frame) => {
-      let issue = `${
-        (frame.resource.type + " #" + frame.resource.id).padEnd(20).blue
-      }${column(frame.title != null ? frame.title : "", 50)}`;
-      return {
-        name:
-          `  ${frame.id}  ${frame.start.clone().format("MMMM Do YYYY HH:mm").green} ${
-            frame.stop ? "to " + frame.stop.clone().format("HH:mm").green : "(running)"}\t` +
-          `${column(frame.project, 50).magenta}${issue}${
-            frame.note != null ? frame.note : ""
-          }`,
-        value: frame.id,
-      };
-    });
-
-    if (lastFramesDetails.length == 0) {
-      Cli.error("No records found.");
-    } else {
-      select({
-        message: "Frame?",
-        default: lastFramesDetails[lastFramesDetails.length - 1].value,
-        choices: lastFramesDetails,
-        pageSize: listSize,
-      }).then((answer) => {
-        if (!Fs.exists(Fs.join(config.frameDir, answer + ".json"))) {
-          Cli.error("record not found.");
-        } else {
-          Fs.open(Fs.join(config.frameDir, answer + ".json"));
+if (!id || program.opts().following) {
+  timekeeper
+    .all()
+    .then(({ frames }) => {
+      frames.sort((a, b) => (a.start.isBefore(b.start) ? -1 : 1));
+      if (id) {
+        let idx = frames.findIndex((fr) => fr.id == id);
+        let following = Number(program.opts().following);
+        if (!Number.isInteger(following)) {
+          following = 0;
         }
+        if (idx >= 0) {
+          frames = frames.slice(idx, idx + following);
+        }
+        else
+        {
+          frames = [];
+        }
+      } else {
+        frames = frames.slice(-listSize); // last listSize frames (one page of inquirer)
+      }
+      let lastFramesDetails = frames.map((frame) => {
+        let issue = `${
+          (frame.resource.type + " #" + frame.resource.id).padEnd(20).blue
+        }${column(frame.title != null ? frame.title : "", 50)}`;
+        return {
+          name:
+            `  ${frame.id}  ${frame.start.clone().format("MMMM Do YYYY HH:mm").green} ${
+              frame.stop
+                ? "to " + frame.stop.clone().format("HH:mm").green
+                : "(running)"
+            }\t` +
+            `${column(frame.project, 50).magenta}${issue}${
+              frame.note != null ? frame.note : ""
+            }`,
+          value: frame.id,
+        };
       });
-    }
+
+      if (lastFramesDetails.length == 0) {
+        Cli.error("No records found.");
+      } else {
+        select({
+          message: "Frame?",
+          default: lastFramesDetails[lastFramesDetails.length - 1].value,
+          choices: lastFramesDetails,
+          pageSize: listSize,
+        }).then((answers) => {
+          answers.forEach((answer) => {
+            if (!Fs.exists(Fs.join(config.frameDir, answer + ".json"))) {
+              Cli.error("record not found.");
+            } else {
+              Fs.open(Fs.join(config.frameDir, answer + ".json"));
+            }
+          });
+        });
+      }
+    })
+    .catch((error) => Cli.error(error));
 } else {
   if (!Fs.exists(Fs.join(config.frameDir, id + ".json")))
     Cli.error("No record found.");
